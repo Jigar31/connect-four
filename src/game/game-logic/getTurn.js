4145caecd9af9e0d54fa668dbd12751321d4406d
Jigar Punjani <jigarpunjani@gmail.com>
2020-10-04 01:34:59 +0530

    removed logs and corrected getTurn function

--- src/game/game-logic/getTurn.js
+++ src/game/game-logic/getTurn.js
@@ -7,42 +7,26 @@ export const getTurn = (
   looser
 ) => {
   let whoStarts = currentGameStartedBy;
-  console.log("turnSetting:", turnSetting);
-  console.log("currentGameStartedBy:", currentGameStartedBy);
-  console.log("player1:", player1);
-  console.log("player2:", player2);
-  console.log("winner:", winner);
-  console.log("looser:", looser);
 
   switch (turnSetting) {
     case 0:
       whoStarts = currentGameStartedBy === player1 ? player2 : player1;
-      console.log("alternate who starts:", whoStarts);
       break;
     case 1:
-      whoStarts = looser ? looser : currentGameStartedBy;
-      console.log("looser who starts:", whoStarts);
+      whoStarts = looser ? looser : player1;
       break;
     case 2:
-      whoStarts = winner ? winner : currentGameStartedBy;
-      console.log("winner who starts:", whoStarts);
+      whoStarts = winner ? winner : player1;
       break;
     case 3:
       whoStarts = player1;
-      console.log("always player1 who starts:", whoStarts);
       break;
     case 4:
       whoStarts = player2;
-      console.log("player2 who starts:", whoStarts);
       break;
     default:
       whoStarts = currentGameStartedBy;
-      console.log("who starts:", whoStarts);
   }
-
-  console.log("who starts:", whoStarts);
-  // console.log("player1", player1);
-  // console.log("player2", player2);
 
   return whoStarts;
 };
