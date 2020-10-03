c47079ea92be51421941846ee209a69c5dbfc836
Jigar Punjani <jigarpunjani@gmail.com>
2020-10-04 00:06:24 +0530

    prod build created

---
+++ build/precache-manifest.a39144340baeb7b07966e5283d38dd68.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2574d4eb777dcc7b565435032a5fc108",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed3ddb88c9fce7478fe0",
+    "url": "/static/css/main.aa9de2f7.chunk.css"
+  },
+  {
+    "revision": "28e0e56d11e6856ee1b4",
+    "url": "/static/js/2.0fbd51bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0fbd51bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed3ddb88c9fce7478fe0",
+    "url": "/static/js/main.321a8508.chunk.js"
+  },
+  {
+    "revision": "bfaf342ef3c0cc951e2b",
+    "url": "/static/js/runtime-main.fd56fe92.js"
+  },
+  {
+    "revision": "529aff0c849dca9e0925561f6654fe57",
+    "url": "/static/media/arrow-left.529aff0c.svg"
+  },
+  {
+    "revision": "1885e09a28bf37d21fc560e2bc879929",
+    "url": "/static/media/connect-four.1885e09a.png"
+  },
+  {
+    "revision": "298f819e6744121cbc95a983b8efcfee",
+    "url": "/static/media/player2-avatar.298f819e.png"
+  }
+]);
